391f4996ca82ad7514ecd0a921d2d830963a989d
gpwclark <gpwclark@gmail.com>
2015-08-30 00:57:52 -0400

    reconfigured pom.xml ro work with new Bootstrap.java class and added boilerplate index.html

--- src/main/java/sparkproject/Bootstrap.java
+++ src/main/java/sparkproject/Bootstrap.java
@@ -2,10 +2,12 @@ package sparkproject;
 import wordgame.*;
 
 import static spark.Spark.get;
-public class Hello {
+import static spark.SparkBase.staticFileLocation;
+public class Bootstrap {
 
     public static void main(String[] args) {
-        get("/", (req, res) -> {
+        staticFileLocation("/public");
+        get("/index.html", (req, res) -> {
             return "Your puzzle: " + core.genPuzz();
         });
     }
