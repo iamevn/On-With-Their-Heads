e01b45a9ee8acea168edfd489e8c0da233834bf1
gpwclark <gpwclark@gmail.com>
2015-09-01 00:37:08 -0400

    Working boilerplate webpage in spark on my machine, docker image not compiling

--- src/main/java/sparkproject/Bootstrap.java
+++ src/main/java/sparkproject/Bootstrap.java
@@ -3,12 +3,50 @@ import wordgame.*;
 
 import static spark.Spark.get;
 import static spark.SparkBase.staticFileLocation;
+import java.io.InputStream;
+import java.lang.StringBuffer;
+import java.lang.Exception;
+
 public class Bootstrap {
 
-    public static void main(String[] args) {
-        get("/", (req, res) -> {
-            return "Your puzzle: " + PuzzleGen.create();
-        });
+  public static void main(String[] args) throws Exception{
+    staticFileLocation("/public"); 
+    get("/", (req, res) -> {
+        //return "Your puzzle: " + PuzzleGen.create();
+        String index = "";
+        try{
+          index = retrieveFile("index.html");
+        }catch(Exception e){
+          e.printStackTrace();
+        }
+
+        return index;
+     });
+  }
+
+  private static String retrieveFile(String file) throws Exception{
+    InputStream in = null;
+    StringBuffer resource = new StringBuffer();
+    int i;
+    char c;
+    file = "public/" + file;
+    try{
+      in = Bootstrap.class.getClassLoader().getResourceAsStream(file);
+
+      while((i=in.read())!=-1 ){
+        c=(char)i;
+        resource.append(c);
+      }
+    }catch(Exception e){
+      e.printStackTrace();
+
+    }finally{
+      if(in != null)
+        in.close();
+
     }
+
+   return resource.toString();
+  }
 
 }
